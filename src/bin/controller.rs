6c67b7743bca218062c1a0d03a52b51dcd917e44
Ole Kliemann <mail@olekliemann.de>
2024-10-23 19:06:57 +0200

    apply and cleanup

--- src/bin/controller.rs
+++ src/bin/controller.rs
@@ -1,6 +1,11 @@
 use kube::{Api, Client};
-use kube::runtime::{controller::{Controller, Action}, watcher};
-use futures::StreamExt;
+use kube::runtime::{
+    controller::{Controller, Action},
+    watcher,
+    finalizer::{finalizer, Event},
+};
+use kube::runtime::finalizer::Error as FinalizerError;
+use futures::{StreamExt};
 use thiserror::Error;
 use co_rust::crd::Chirpstack;
 use std::sync::Arc;
@@ -10,13 +15,39 @@ use env_logger;
 #[derive(Debug, Error)]
 enum Error {}
 
-async fn reconcile(g: Arc<Chirpstack>, _ctx: Arc<()>) -> Result<Action, Error> {
-    log::info!("{g:?}");
+async fn apply(chirpstack: Arc<Chirpstack>) -> Result<Action, Error> {
+    log::info!("APPLY {chirpstack:?}");
     Ok(Action::requeue(Duration::from_secs(300)))
 }
 
-fn error_policy(_obj: Arc<Chirpstack>, _error: &Error, _ctx: Arc<()>) -> Action {
+async fn cleanup(chirpstack: Arc<Chirpstack>) -> Result<Action, Error> {
+    log::info!("CLEANUP {chirpstack:?}");
+    Ok(Action::requeue(Duration::from_secs(300)))
+}
+
+async fn reconcile(chirpstack: Arc<Chirpstack>, context: Arc<Context>) -> Result<Action, FinalizerError<Error>> {
+    log::info!("{chirpstack:?}");
+    let client = context.client.clone();
+    let api: Api<Chirpstack> = Api::namespaced(client, chirpstack.metadata.namespace.as_deref().or(Some("default")).unwrap());
+    finalizer(
+        &api,
+        "chirpstack-finalizer",
+        chirpstack,
+        |event| async {
+            match event {
+                Event::Apply(chirpstack) => apply(chirpstack).await,
+                Event::Cleanup(chirpstack) => cleanup(chirpstack).await,
+            }
+        },
+    ).await
+}
+
+fn error_policy(_obj: Arc<Chirpstack>, _error: &FinalizerError<Error>, _ctx: Arc<Context>) -> Action {
     Action::requeue(Duration::from_secs(60))
+}
+
+struct Context {
+    client: Client,
 }
 
 #[tokio::main]
@@ -24,9 +55,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
     log::info!("starting...");
     let client = Client::try_default().await?;
-    let context = Arc::new(());
-    let root_objects: Api<Chirpstack> = Api::all(client);
-    Controller::new(root_objects, watcher::Config::default())
+    let api: Api<Chirpstack> = Api::all(client.clone());
+    let context = Arc::new(
+        Context{
+            client: client,
+        }
+    );
+    Controller::new(api, watcher::Config::default())
         //.owns(watcher::Config::default())
         .run(reconcile, error_policy, context)
         .for_each(|res| async move {
